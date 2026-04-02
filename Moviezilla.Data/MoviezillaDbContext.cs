3da865a08b17d7f8085c778f7400bfa822e3fdb4
Razhagal <hpetrovv@gmail.com>
2026-04-02 21:47:27 +0300

    Initial project setup

---
+++ Moviezilla.Data/MoviezillaDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Moviezilla.Data;
+
+public class MoviezillaDbContext(DbContextOptions<MoviezillaDbContext> options) : IdentityDbContext(options)
+{
+}
